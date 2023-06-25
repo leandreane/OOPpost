49f0b03218a7bb819c8801042d0ffd05062ff352
leandreane <monk-86@inbox.ru>
2023-06-25 15:18:47 +0300

    first commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+public class Post {
+
+    public String name; // поле для имени
+    public String passport; // поле для паспорта
+    public String patronymic; // поле для отчества
+    public String phone; // поле для телефона
+    public String surname; // роле для фамилии
+    public boolean subscription; // поле для согласия
+    public FormDate birthday; // поле для дня рождения
+
+}
